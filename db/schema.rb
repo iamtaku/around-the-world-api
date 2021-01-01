0cf995318a72e18a943b460f046d0c51af68f9c1
Takuhei Mizutani <takuheim1011@gmail.com>
2021-01-01 11:11:05 +0900

    created seed logic

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_31_134959) do
+ActiveRecord::Schema.define(version: 2021_01_01_020918) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,9 +27,10 @@ ActiveRecord::Schema.define(version: 2020_12_31_134959) do
 
   create_table "people", force: :cascade do |t|
     t.string "name"
-    t.integer "fb_id"
+    t.bigint "fb_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "home"
   end
 
   create_table "places", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,55 @@
 require 'json'
 
-# file = File.open 'test.json'
-# data = JSON.load file
-Person.destroy_all
-Place.destroy_all
+puts "Destoying all lived ins..."
 LivedIn.destroy_all
+puts "Destorying all places..."
+Place.destroy_all
+puts "Destroying all people"
+Person.destroy_all
 
-person = Person.create!(name:'taku')
-place = Place.create!(location:'Gifu')
-place2 = Place.create!(location:'Tokyo')
-lived_in = LivedIn.new(year: 2020)
-lived_in.place = place
-lived_in.person = person
-lived_in.save
-person.lived_ins
+file = File.read 'db/test.json'
+# data = JSON.load file
+data = JSON.parse file
+# puts data['key'].each do |hash|
+#   puts hash
+# end
+# test =  json_data['key'].first
+# p JSON.parse test
+
+
+
+data['key'].each do |item|
+  hash = JSON.parse item
+
+  person = Person.create!(name: hash['name'], fb_id: hash['fb_id'])
+  # puts hash['living']
+  # test = hash["living"]
+  p hash
+  # p hash['living']['lived_in']
+  next if hash['living']['lived_in'].nil?
+
+  hash['living']['lived_in'].each do |place|
+      place = Place.create!(location: place['place'])
+      livedIn = LivedIn.new(year: place['year'])
+      livedIn.place = place
+      livedIn.person = person
+      livedIn.save
+  end
+  next if hash['living']['home'].nil?
+  person.home = hash['living']['home']
+  person.save
+
+end
+puts "Created #{Person.all.count} people!"
+# Person.destroy_all
+# Place.destroy_all
+# LivedIn.destroy_all
+
+# person = Person.create!(name:'taku')
+# place = Place.create!(location:'Gifu')
+# place2 = Place.create!(location:'Tokyo')
+# lived_in = LivedIn.new(year: 2020)
+# lived_in.place = place
+# lived_in.person = person
+# lived_in.save
+# person.lived_ins

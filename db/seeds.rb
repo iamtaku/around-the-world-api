69e9deb0a8f22304189b1bb1916361e413742758
Takuhei Mizutani <takuheim1011@gmail.com>
2021-01-02 14:38:14 +0900

    changed seed logic temporarily

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,19 +21,20 @@ progressbar = ProgressBar.create(
 
 data['key'].each do |item|
   hash = JSON.parse item
-  person = Person.create!(name: hash['name'], fb_id: hash['fb_id'])
+  # person = Person.create!(name: hash['name'], fb_id: hash['fb_id'])
   next if hash['living']['lived_in'].nil?
 
   hash['living']['lived_in'].each do |place|
     place = Place.create!(location: place['place'])
-    lived_in = LivedIn.new(year: place['year'])
-    lived_in.place = place
-    lived_in.person = person
-    progressbar.increment if lived_in.save
+    # lived_in = LivedIn.new(year: place['year'])
+    # lived_in.place = place
+    # lived_in.person = person
+    # progressbar.increment if lived_in.save
+    progressbar.increment
   end
   next if hash['living']['home'].nil?
 
-  person.home = Place.create!(location: hash['living']['home']).id
-  person.save
+  Place.create!(location: hash['living']['home'])
+  # person.save
 end
 puts "\n ... \n\n Created #{Person.all.count} people!"
